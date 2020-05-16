f9ca53e7845ebf9cb4e0fcc702f0fad779244932
Valdirene Elaine <valdirene.elaine@gmail.com>
2020-05-16 09:06:40 -0400

    ajustando o estilo

--- exercicios/lista-convidados/script.js
+++ exercicios/lista-convidados/script.js
@@ -2,11 +2,19 @@ let nomesConvidados = ["Jão da Silva", "Maria Da Silva"]
 
 function adicionar() {
     let nome = document.getElementById("nomeConvidado").value
+    let idade = document.getElementById("idadeConvidado").value
+    let email = document.getElementById("emailConvidado").value
 
     if (nome == "") {
         alert("Preencha o nome do Convidado")
+    } else
+    if (idade == "") {
+        alert("Preencha a idade do Convidado")
+    } else
+    if (email == "") {
+        alert("Preencha o email do Convidado")
     } else {
-        nomesConvidados.push(nome)
+        nomesConvidados.push([nome, idade, email])
         atualizarLista()
     }
 }
